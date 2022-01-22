fb1d73a035ce8bee221f76397e06f7ee5f2a6b16
Rahul Vishwakarma <rvk.ahul@gmail.com>
2022-01-22 18:13:37 +0530

    Find missing number in a Array

---
+++ Java/src/main/java/practice/FindMissingNumberInArray.java
@@ -0,0 +1,21 @@
+package practice;
+
+public class FindMissingNumberInArray {
+
+    private static int findMissingNumber(int arr[], int n){
+        int sumOfNNum = n * (n + 1) / 2;
+        int actualSum = 0;
+
+        for (int i : arr) actualSum += i;
+
+        return sumOfNNum - actualSum;
+    }
+
+    public FindMissingNumberInArray() {
+        int arr[] = new int[] {1, 2, 3, 5};
+        System.out.println("#######################################");
+        System.out.println("Find Missing Number in a Array");
+        System.out.println(findMissingNumber(arr, arr.length + 1));
+        System.out.println("#######################################");
+    }
+}
