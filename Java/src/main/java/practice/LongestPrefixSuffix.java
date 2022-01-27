85faa8837197f1d8f65cf5ad847edddf8c5a435e
rvishwakarma <rvishwakarma@quotient.com>
2022-01-27 18:59:42 +0530

    Longest Prefix Suffix

---
+++ Java/src/main/java/practice/LongestPrefixSuffix.java
@@ -0,0 +1,40 @@
+package practice;
+
+// https://www.geeksforgeeks.org/longest-prefix-also-suffix/
+public class LongestPrefixSuffix {
+
+    private static int lps(String s) {
+        int[] mat = new int[s.length()];
+        int i = 0, j = 1;
+        while(j < s.length())
+        {
+            if(s.charAt(j) == s.charAt(i))
+            {
+                mat[j] = i+1;
+                i++;
+                j++;
+            }
+            else
+            {
+                if(i==0)
+                    j++;
+                else
+                    i = mat[i-1];
+
+            }
+        }
+
+        return mat[s.length()-1];
+    }
+
+    public LongestPrefixSuffix() {
+        System.out.println("##############################");
+        System.out.println("Longest Prefix Suffix");
+        String S = "yycgicgyyycg";
+        System.out.println(S.length());
+        System.out.println("S: " + S + " Length of lps: " + lps(S));
+
+        System.out.println("##############################");
+    }
+
+}
--- Java/src/main/java/practice/Main.java
+++ Java/src/main/java/practice/Main.java
@@ -1,19 +1,25 @@
 package practice;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        new ConwaysGameOfLife();
-        new BowlingSingle();
-        new LongestPalindrome();
-        new FindMissingNumberInArray();
-        new TrailingZeroesInFactorial();
-        new SimpleFraction();
-        new NthNaturalNumber();
-        new ReverseWordsInAString();
-        new LongestCommonPrefixInArray();
-        new RomanNumberToInteger();
+//        new ConwaysGameOfLife();
+//        new BowlingSingle();
+//        new LongestPalindrome();
+//        new FindMissingNumberInArray();
+//        new TrailingZeroesInFactorial();
+//        new SimpleFraction();
+//        new NthNaturalNumber();
+//        new ReverseWordsInAString();
+//        new LongestCommonPrefixInArray();
+//        new RomanNumberToInteger();
+//        new NextHigherPalindrome();
+        new LongestPrefixSuffix();
 
     }
 }
