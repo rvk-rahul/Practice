230f248540a61836e5815a14f1c37b0006945ea7
rvishwakarma <rvishwakarma@quotient.com>
2022-02-01 23:31:10 +0530

    Look And Say Pattern

--- Java/src/main/java/practice/Main.java
+++ Java/src/main/java/practice/Main.java
@@ -3,19 +3,19 @@ package practice;
 public class Main {
 
     public static void main(String[] args) {
-//        new ConwaysGameOfLife();
-//        new BowlingSingle();
-//        new LongestPalindrome();
-//        new FindMissingNumberInArray();
-//        new TrailingZeroesInFactorial();
-//        new SimpleFraction();
-//        new NthNaturalNumber();
-//        new ReverseWordsInAString();
-//        new LongestCommonPrefixInArray();
-//        new RomanNumberToInteger();
-//        new NextHigherPalindrome();
-//        new LongestPrefixSuffix();
-//        new FindSubStringInString();
+        new ConwaysGameOfLife();
+        new BowlingSingle();
+        new LongestPalindrome();
+        new FindMissingNumberInArray();
+        new TrailingZeroesInFactorial();
+        new SimpleFraction();
+        new NthNaturalNumber();
+        new ReverseWordsInAString();
+        new LongestCommonPrefixInArray();
+        new RomanNumberToInteger();
+        new NextHigherPalindrome();
+        new LongestPrefixSuffix();
+        new FindSubStringInString();
         new LookAndSayPattern();
     }
 }
